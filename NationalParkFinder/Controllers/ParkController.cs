490188badcc4ecb46e0c6d4b1462b43af3add57f
ethan <ethanbjansen@gmail.com>
2023-03-17 13:54:34 -0500

    Added User Profile

---
+++ NationalParkFinder/Controllers/ParkController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NationalParkFinder.Models;
+
+namespace NationalParkFinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkController : ControllerBase
+    {
+        [HttpGet("getParks")]
+        public List<Park> GetParks()
+        {
+            return ParkDAL.GetPark();
+        }
+        [HttpGet("getParksByName")]
+        public List<Park> GetParksByName(string _parkName)
+        {
+            List<Park> allParks = ParkDAL.GetPark();
+            List<Park> resultParks= new List<Park>();
+            foreach(Park park in allParks)
+            {
+                foreach(Datum d in park.data)
+                {
+                    if (d.fullName.Contains(_parkName))
+                        {
+                        resultParks.Add(park);
+                    }
+                }
+            }
+            return resultParks;
+            /*allParks.Where(park => park.data.Where(p => p.fullName.Contains(_parkName));*/
+        }
+        [HttpGet("getParksByName")]
+        public List<Park> GetParksByName(string _parkName)
+        {
+            List<Park> allParks = ParkDAL.GetPark();
+            List<Park> resultParks = new List<Park>();
+            foreach (Park park in allParks)
+            {
+                foreach (Datum d in park.data)
+                {
+                    if (d.fullName.Contains(_parkName))
+                    {
+                        resultParks.Add(park);
+                    }
+                }
+            }
+            return resultParks;
+            /*allParks.Where(park => park.data.Where(p => p.fullName.Contains(_parkName));*/
+        }
+        [HttpGet("getParksByActivity")]
+        public List<Park> GetParksByActivities(string _allResults) { 
+            string[] results= _allResults.Split(',');
+            
+            
+            List<Park> allParks = ParkDAL.GetPark();
+            List<Park> resultParks = new List<Park>();
+            foreach(string r in results)
+            {
+                List<Park> parks = GetParksByActivity(r, allParks);
+                foreach(Park park in parks)
+                {
+                    resultParks.Add(park);
+                }
+
+            }
+            return resultParks.Distinct().ToList();
+
+        }
+
+        public List<Park> GetParksByActivity(string _name, List<Park> _parks)
+        {
+            List<Park> resultParks = new List<Park>();
+            foreach(Park park in _parks)
+            {
+                foreach(Datum d in park.data)
+                {
+                    foreach(Activity a in d.activities)
+                    {
+                        if(a.name == _name)
+                        {
+                            resultParks.Add(park);
+                        }
+                    }
+                }
+            }
+            return resultParks;
+        }
+    }
+}
--- NationalParkFinder/Controllers/UserProfileController.cs
+++ NationalParkFinder/Controllers/UserProfileController.cs
@@ -9,7 +9,49 @@ namespace NationalParkFinder.Controllers
     public class UserProfileController : ControllerBase
     {
         NpsContext dbContext = new NpsContext();
-        [HttpGet("userProfile")]
-        private UserProfile
+        [HttpGet("getUserProfile")]
+        private UserProfile getUserProfile(string _googleId)
+        {
+            return dbContext.UserProfiles.FirstOrDefault(p => p.GoogleId == _googleId);
+        }
+        [HttpPut("updateUserProfile")]
+        private UserProfile updateUserProfile(int _profileId, string _googleId, string _region, string _username, string _homeTown, string _activities, string _lodging, string _style) 
+        {
+            UserProfile currentProfile = dbContext.UserProfiles.FirstOrDefault(p => p.Id == _profileId);
+            currentProfile.GoogleId = _googleId;
+            currentProfile.Region = _region;
+            currentProfile.UserName= _username;
+            currentProfile.Hometown= _homeTown;
+            currentProfile.Activities= _activities;
+            currentProfile.Lodging= _lodging;
+            currentProfile.Style= _style;
+            dbContext.SaveChanges();
+            return currentProfile;
+        }
+        [HttpPost("createUserProfile")]
+        private UserProfile createUserProfile(string _googleId, string _region, string _username, string _homeTown, string _activities, string _lodging, string _style)
+        {
+            if(dbContext.UserProfiles.Any(p => p.GoogleId == _googleId))
+            {
+                return null;
+            }
+            else
+            {
+                UserProfile newProfile = new UserProfile()
+                {
+                    GoogleId = _googleId,
+                    Region = _region,
+                    UserName = _username,
+                    Hometown = _homeTown,
+                    Activities = _activities,
+                    Lodging = _lodging,
+                    Style = _style
+                };
+                dbContext.UserProfiles.Add(newProfile);
+                dbContext.SaveChanges();
+                return newProfile;
+            }
+             
+        }
     }
 }
