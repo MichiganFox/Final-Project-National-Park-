c59e70e30c284278b4ac75a6614c4971e1d56345
belle <belle.baxley@gmail.com>
2023-03-21 16:23:05 -0400

    added user profile form

--- NationalParkFinder/ClientApp/src/app/Component/profile-form/profile-form.component.ts
+++ NationalParkFinder/ClientApp/src/app/Component/profile-form/profile-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { UserProfileService } from 'src/app/Service/user-profile.service';
+import { UserProfile } from '../../Model/user-profile';
 
 @Component({
   selector: 'app-profile-form',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-form.component.css']
 })
 export class ProfileFormComponent {
+@Output() submitted = new EventEmitter<UserProfile>();
 
+constructor(private userService:UserProfileService, private authService:SocialAuthService){};
+
+newUser:UserProfile = {} as UserProfile;
+
+    user: SocialUser = {} as SocialUser;
+    loggedIn: boolean = false;
+    
+    ngOnInit(): void {
+
+      this.authService.authState.subscribe((user) => {
+        this.user = user;
+        this.loggedIn = (user != null);
+      });
+    }
+
+createUserProfile():void{
+  let result:UserProfile = {
+    googleId: this.user.id,
+    region: this.newUser.region,
+    userName: this.newUser.userName,
+    homeTown: this.newUser.homeTown,
+    activities: this.newUser.activities,
+    lodging: this.newUser.lodging,
+    style: this.newUser.style
+  };
+
+  this.userService.createUserProfile(result).subscribe((result:UserProfile) => {
+    console.log(result);
+  });
+
+}
 }
